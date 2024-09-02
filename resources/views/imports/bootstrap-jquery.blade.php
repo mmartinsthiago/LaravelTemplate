17fba9ba7255b55a22e664b2fb4e571d04df338e
Thiago Mmendes <mmartinsthiago@gmail.com>
2024-09-02 16:00:13 -0300

    Ready to use

---
+++ resources/views/imports/bootstrap-jquery.blade.php
@@ -0,0 +1,7 @@
+<!-- Includes Bootstrap -->
+<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+<script src="{{ asset('js/popper.min.js') }}" defer></script>
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}" defer></script>
+
+<!-- Includes jQuery -->
+<script src="{{ asset('js/jquery.min.js') }}"></script>
