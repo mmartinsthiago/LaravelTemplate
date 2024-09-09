917e5e08d980a24e8d0b2535b6cd16ca0d0c39c9
Thiago Mmendes <mmartinsthiago@gmail.com>
2024-09-09 13:34:17 -0300

    Some touches

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# LaravelTemplate
-This templates simplifies and set ready to use Bootstrap and JQuery with Laravel.
+# laravel-bootstrap-jquery
+- This templates simplifies and set ready to use Laravel, laravelcollective/html, Bootstrap, @popperjs/core, Bootstrap icons, sass, JQuery in your Laravel project.
+- After executing the steps in *"How to use?"*, you will have and *".env"* file with the configurations setted in *".env.example"* and a *blade* with the name *"test.blade.php"* to verify if all is working.
+- There is a file in *resources/views/lang/en.json* as model for translations in your project.
 
 ## What is needed?
 - git;
@@ -11,7 +13,6 @@ This templates simplifies and set ready to use Bootstrap and JQuery with Laravel
 ## How to use?
 - after you have cloned the repository, access typing: ```$ cd directory-name```;
 - paste this in your terminal: ```$ sudo chmod +x bootstrap-jquery.sh```;
+- input your password;
 - paste this in your terminal: ```$ ./bootstrap-jquery.sh```;
 - paste this inside the blade with your ```head.../head```: ```@include('imports.bootstrap-jquery')```.
-
-With this steps you will be able to use classes from bootstrap, bootstrap icons, jquery's codes and sass configuration.
--- bootstrap-jquery.sh
+++ bootstrap-jquery.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
 
-echo "=========================================="
-echo "Installing Laravel dependencies ..."
+clear
+echo "=================================================="
+echo "=================================================="
+echo "=================================================="
+echo "=================================================="
+echo "Welcome to Laravel, Bootstrap and JQuery template!"
+echo "\n"
+sleep 3
+echo "Here will be installed the Laravel and npm dependecies:"
+sleep 1
+echo "1) Laravel and laravelcollective/html;"
+sleep 1
+echo "2) npm dependencies, bootstrap, @popperjs/core, bootstrap icons and jquery;"
+sleep 1
+echo "3) copy the '.env.example' to '.env';"
+sleep 1
+echo "4) generate the artisan key."
+sleep 1
+echo "The instalation will begin in 3 seconds:"
+echo "=================================================="
+echo "=================================================="
+echo "=================================================="
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+clear
+
+# Laravel dependecies
 composer install
 composer dump-autoload
-echo "------------------------------------------"
-echo "Laravel dependencies installed!"
-echo "=========================================="
-sleep 3
-
 
 # Install Bootstrap and jQuery
-echo "=========================================="
-echo "Installing Bootstrap and jQuery ..."
 npm install
 npm install bootstrap @popperjs/core jquery bootstrap-icons
 npm install sass
-echo "------------------------------------------"
-echo "Bootstrap and jQuery installed!"
-echo "=========================================="
-sleep 3
 
 # Copy .env.example
-echo "=========================================="
-echo "Copying .env.example ..."
-cp .env.example .env
-echo "------------------------------------------"
-echo ".env created!"
-echo "------------------------------------------"
-echo "Generating key ..."
-php artisan key:generate
-echo "------------------------------------------"
-echo "Key generated!"
-echo "=========================================="
-sleep 3
+cp -f .env.example .env
 
+# Generate artisan key
+php artisan key:generate
+
+clear
 echo "=========================================="
 echo "Your project is ready for development!"
 sleep 2
